c13ca0974ab9b0c786342903abb1775cc545ef2d
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-04-10 13:28:13 +0700

    fix taiwind css ui

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -146,7 +146,7 @@ const Header = () => {
       <div className="w-full relative shadow-lg">
         <div className=" relative mx-auto max-w-screen-2xl ">
           {/* Logo */}
-          <div className=" xl:absolute  relative w-full  xl:left-[4%] h-full shadow-sm  px-2  xl:max-w-[240px] bottom-[112%] z-40">
+          <div className="xl:absolute relative w-full xl:left-[4%] h-full shadow-sm px-2 xl:max-w-[240px] bottom-[112%] z-40">
             <div className="bg-white text-center sticky">
               <button onClick={() => navigate("/")}>
                 <img
