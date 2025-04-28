10193edc76e28c97958a876e4930a22a90114157
Azure2212 <dasanbob22122002@gmail.com>
2025-04-28 16:58:21 +0700

    update footer

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -71,7 +71,7 @@ function ContactPage() {
                 </div>
                 <div className="flex items-start">
                   <IoLocationSharp className="w-6 h-6 text-brandSecondary mr-1 font-semibold" />
-                  <div className="flex Item-center gap-2">
+                  <div className="flex flex-wrap Item-center gap-2">
                     <span className="font-semibold">
                       {t("contactPage.adddresslabel")}{" "}
                     </span>
